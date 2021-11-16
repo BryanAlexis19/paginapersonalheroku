fe7174fadf61779d9c75593980059b7cfb965f51
BryanAlexis19 <librale19@gmail.com>
2021-11-15 21:34:55 -0500

    d0

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    Experiencia.findAll()
+    .then((exp) => {
+        console.log(exp)
+        res.render('index', {
+            experiencias:exp,
+            tituloexp: 'EXPERIENCIA LABORAL'
+        })
+        console.log('hola mundo')
+    })
+    
+})
+
+app.use(express.static('static'))
+
+app.listen(port, () => {
+  console.log(`Corriendo en http://localhost:${port}`)
+})
+
+//============SEQUELIZE====================
+//-----Conectar a la bd
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize({
+    dialect:'sqlite',
+    storage:'./database.sqlite'
+})
+
+sequelize.authenticate()
+.then(()=>{
+    console.log("conexion establecida");
+})
+.catch(err=>{
+    console.log("error al conectarse");
+})
+
+//-----Creacion de un modelo
+const Experiencia = sequelize.define(
+    'experiencia',
+    {
+        puesto:Sequelize.STRING,        
+        empresa:Sequelize.STRING,
+        descripcion:Sequelize.STRING,
+        periodo:Sequelize.STRING
+    }
+  );
+  
+  //migración y poblado de data
+  sequelize.sync({force:true})
+  .then
+  (
+    ()=>
+    {
+        console.log("BD y tabla creada")
+        Experiencia.bulkCreate(
+            [
+                {puesto:'FullStack developer', empresa:'TI CONSULTORES', descripcion:'Desarrollador BackEnd y FrontEnd', periodo:'Diciembre 2017 - Julio 2018'},
+                {puesto:'Front-end Developer', empresa:'FREELANCE', descripcion:'Desarrollador FrontEnd', periodo:'Agosto 2018 - Abril 2019'},
+                {puesto:'Back-end Developer', empresa:'AGP CORP', descripcion:'Database Manager', periodo:'Marzo 2019 - Actualidad'},
+
+            ]).then(function(){
+                return Experiencia.findAll();
+            }).then(function(experiencia){
+                console.log(experiencia)
+            })
+    }
+  )
